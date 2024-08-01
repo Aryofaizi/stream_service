084c57f00c1aacdf31127546197c6fd7748c0703
aryo faizi <aryofaizi@gmail.com>
2024-08-01 18:15:23 +0330

    obtain refresh and access tokens defined in setUp method to be availabe in the class level

--- core/tests.py
+++ core/tests.py
@@ -17,6 +17,18 @@ class CoreTest(TestCase):
         cls.user.set_password(cls.USER_PASSWORD) # Correctly hash the password
         cls.user.save() # Save the user with the hashed password
         
+    def setUp(self):
+        # First, obtain a refresh token from login endpoint
+        login_url = reverse("jwt-create")
+        login_payload = {
+            "username": self.USER_USERNAME,
+            "password": self.USER_PASSWORD,
+        }
+        login_response = self.client.post(path=login_url, data=login_payload, content_type="application/json")
+        self.refresh_token = login_response.json().get("refresh")
+        self.access_token = login_response.json().get("access")
+        
+        
     
     def test_register_user(self):
         """test for the register user endpoint
@@ -51,20 +63,11 @@ class CoreTest(TestCase):
     def test_refresh_token(self):
         """test for refresh token endpoint 
         i.e. obtain an access token."""
-        
-        # First, obtain a refresh token from login endpoint
-        login_url = reverse("jwt-create")
-        login_payload = {
-            "username": self.USER_USERNAME,
-            "password": self.USER_PASSWORD,
-        }
-        login_response = self.client.post(path=login_url, data=login_payload, content_type="application/json")
-        refresh_token = login_response.json().get("refresh")
 
         # Now, use the refresh token to obtain a new access token
         url = reverse("jwt-refresh")
         payload = {
-            "refresh" : refresh_token
+            "refresh" : self.refresh_token
         }
         response = self.client.post(path=url, data=payload, content_type="application/json")
         self.assertEqual(response.status_code, 200)
