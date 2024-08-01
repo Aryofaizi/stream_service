fd303ce8c051b2ee326a28667e227b8c6165d0d7
aryo faizi <aryofaizi@gmail.com>
2024-08-01 15:01:52 +0330

    Add test for user registration endpoint in CoreTest
    - Added a test method `test_register_user` to `CoreTest` class in `test.py`
    - The test checks the user registration functionality using the `customuser-list` endpoint
    - Validates the response status code and presence of "username" and "email" in the response

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CoreTest(TestCase):
+    
+    def test_register_user(self):
+        """test for the register user endpoint
+        i.e. make a new CustomUser"""
+        url = reverse("customuser-list")
+        payload = {
+            "username": "test",
+            "password": "usertest1234",
+            "email": "test@gmail.com",
+        }
+        response = self.client.post(path=url, data=payload, content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("username", response.json())
+        self.assertIn("email", response.json())
+        
