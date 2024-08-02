010e9fbd3764f63b962bfcd3b6b53a08bc1b48f1
aryo faizi <aryofaizi@gmail.com>
2024-08-02 19:29:04 +0330

    IsAdminOrReadOnly is created in content/permissions.py and the ContentViewSet is updated with the new permission created as permission_classes

--- content/views.py
+++ content/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from . import serializers
 from . import models
+from .permissions import IsAdminOrReadOnly
+
 
 class ContentViewSet(ModelViewSet):
     serializer_class = serializers.ContentSerializer
     queryset = models.Content.objects.all()
-    
+    permission_classes = [IsAdminOrReadOnly]
