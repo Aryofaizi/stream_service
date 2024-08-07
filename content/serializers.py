c3343a01061e8bb82cd90d84cc34c0fee3b4fbc1
aryo faizi <aryofaizi@gmail.com>
2024-08-07 13:21:40 +0330

    ContentSerializer modified - id is added to the fields - create and update method overwrote i.e the nested fields functionality is implemented

--- content/serializers.py
+++ content/serializers.py
@@ -11,9 +11,44 @@ class ContentSerializer(serializers.ModelSerializer):
     genre = GenreSerializer(many=True)
     class Meta:
         model = models.Content
-        fields = ["title", "description", "release_date",
+        fields = ["id", "title", "description", "release_date",
                   "genre", "category", "rate","price"]
         
+    def create(self, validated_data):
+        genre_list = validated_data.pop("genre")
+        
+        content = models.Content.objects.create(**validated_data)
+        
+        for genre_data in genre_list:
+            genre = models.Genre.objects.get(title=genre_data.get("title"))
+            content.genre.add(genre)
+            
+        content.save()
+        return content
+    
+    def update(self, instance, validated_data):
+        genre_list = validated_data.pop("genre", None)
+        
+        
+        # update non-many-to-many fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+            
+          # Save the instance to ensure it's updated before modifying many-to-many field
+        instance.save()
+        
+        
+        if genre_list is not None:
+            # Clear the existing genres
+            instance.genre.clear() 
+            
+        # Retrieve and add new Genre instances
+        for genre_data in genre_list:
+            genre = models.Genre.objects.get(title=genre_data.get("title"))
+            instance.genre.add(genre)
+        # Return the updated instance
+        return instance
+         
         
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta: 
