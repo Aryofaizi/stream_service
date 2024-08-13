3406bdf49f1e3c3393c19582e2b08251d6f7abb2
aryo faizi <aryofaizi@gmail.com>
2024-08-13 14:06:02 +0330

    cart admin, models, migrations created

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    """represent Cart Model in AdminPanel to be editable."""
+    model = Cart
+    list_display = [field.name for field in model._meta.fields]
+    
+    
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    """represent CartItem Model in AdminPanel to be editable."""
+    model = CartItem
+    list_display = [field.name for field in model._meta.fields]
+    
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uuid import uuid4
+from content.models import Content
 
-# Create your models here.
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    content = models.ForeignKey(Content, on_delete=models.CASCADE, related_name="cart_items")
+    quantity = models.PositiveSmallIntegerField()
+    
+    class Meta:
+        unique_together = [["cart", "content"]]
