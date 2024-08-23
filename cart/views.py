7be3579e4488efce7b68b2da425223d19d9716c9
aryo faizi <aryofaizi@gmail.com>
2024-08-23 21:23:05 +0330

    DestroyModelMixin imported in cart views.py and the CartViewSet is now on capable of deletion process

--- cart/views.py
+++ cart/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CartSerializer, CartItemSerializer, AddCartItemSerializer
 from .models import Cart, CartItem
-from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
 from django.db.models import Prefetch
 
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin, GenericViewSet,
+                  DestroyModelMixin):
     serializer_class = CartSerializer
     queryset = Cart.objects.all().prefetch_related(Prefetch(
         "items", CartItem.objects.select_related("content")
