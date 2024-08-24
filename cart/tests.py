11e0afe94eebc6868cb78a6527aa267243649e60
aryo faizi <aryofaizi@gmail.com>
2024-08-24 17:44:41 +0330

    cart urls modified , cart tests created urls : the basename specified as kwarg in the urls tests : the test_cart_create test is implemented

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+
+class CartTest(TestCase):
+    """tests for the cart app."""
+        
+    
+    def test_cart_create(self):
+        """Tests if the creat cart endpoint returns 201 status code."""
+        url = reverse("cart-list")
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        
+    
+        
+
--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework_nested.routers import NestedDefaultRouter
 
 router = DefaultRouter()
-router.register("", views.CartViewSet, "cart")
+router.register("", views.CartViewSet, basename="cart")
 
 cart_item_router = NestedDefaultRouter(router, "", lookup="cart")
 cart_item_router.register("items", views.CartItemViewSet, "cart_item")
