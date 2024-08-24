67af46a832aaa4c13a1fb1d83e0fbeaf360406b2
aryo faizi <aryofaizi@gmail.com>
2024-08-24 19:41:33 +0330

    test_cart_item_delete implemented

--- cart/tests.py
+++ cart/tests.py
@@ -86,4 +86,12 @@ class CartTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
-    
+    def test_cart_item_delete(self):
+        """Tetst if the deletion process of the 
+        specified cart item."""
+        url = reverse("cart-item-detail", kwargs={
+            "cart_pk": self.cart.id,
+            "pk": self.cart_item.id
+            })
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
