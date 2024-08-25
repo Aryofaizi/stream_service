f42d8544045e13dd583295a3313dcfe0213c50a3
aryo faizi <aryofaizi@gmail.com>
2024-08-25 21:54:44 +0330

    admin classes and models with migrations for Order, OrderItem created

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    list_display = [field.name for field in model._meta.fields]
+    
+    
+    
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    model = OrderItem
+    list_display = [field.name for field in model._meta.fields]
--- order/models.py
+++ order/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from config.settings import AUTH_USER_MODEL
+from content.models import Content
 
-# Create your models here.
+
+class Order(models.Model):
+    ORDER_STATUS_PAID = 'p'
+    ORDER_STATUS_UNPAID = 'u'
+    ORDER_STATUS_CANCELED = 'c'
+    ORDER_STATUS = [
+        (ORDER_STATUS_PAID,'Paid'),
+        (ORDER_STATUS_UNPAID,'Unpaid'),
+        (ORDER_STATUS_CANCELED,'Canceled'),
+    ]
+    
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='orders')
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+    
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    content = models.ForeignKey(Content, on_delete=models.PROTECT, related_name='order_items')
+    unit_price = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = [['order', 'content']]
+    
