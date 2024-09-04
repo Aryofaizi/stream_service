6c74f92a57a2a2e3e9c8e1f8d749c4d629e315c7
aryo faizi <aryofaizi@gmail.com>
2024-09-04 17:26:00 +0330

    OrderItemViewSet updated with new get_queryset approach now on the admin users are able to see all orderitems but regular users can  just check there own order items

--- order/views.py
+++ order/views.py
@@ -4,6 +4,7 @@ from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemSerializer, AddOrderItemSerilizer
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Prefetch
+from rest_framework.exceptions import NotFound
 
 
 class OrderViewSet(ModelViewSet):
@@ -26,6 +27,7 @@ class OrderViewSet(ModelViewSet):
 class OrderItemViewSet(ModelViewSet):
     http_method_names = ["get", "post", "options", "head", "delete"]
     serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
     
     def get_serializer_class(self):
         if self.request.method == "POST":
@@ -36,6 +38,23 @@ class OrderItemViewSet(ModelViewSet):
         context = {"order_id":self.kwargs["order_pk"]}
         return context
     
+    
     def get_queryset(self):
         order_id = self.kwargs["order_pk"]
-        return OrderItem.objects.filter(order=order_id).prefetch_related("content__genre")
+        user = self.request.user
+
+        # If the user is an admin, return all OrderItems for the specified order
+        if user.is_staff:  # Check if the user is an admin
+            try:
+                order = Order.objects.get(pk=order_id)
+            except Order.DoesNotExist: 
+                raise NotFound("Order not found")    
+            return OrderItem.objects.filter(order=order).prefetch_related("content__genre")
+        # For regular users, ensure they only access their own orders
+        try:
+            order = Order.objects.get(pk=order_id, user=user)
+        except Order.DoesNotExist:
+            raise NotFound("Order not found or you do not have permission to access it.")
+
+        return OrderItem.objects.filter(order=order).prefetch_related("content__genre")
+    
