0edb93f3b7ddbd6bddea4cdb41bb99dd596d63e2
aryo faizi <aryofaizi@gmail.com>
2024-09-05 15:49:14 +0330

    test_order_create is implemented

--- order/tests.py
+++ order/tests.py
@@ -6,12 +6,21 @@ from rest_framework import status
 class OrderTest(AuthMixin, TestCase):
     """A test class for order app."""
     
+    def setUp(self):
+        self.headers ={
+            "Authorization": f"{self.AUTH_TOKEN_PREFIX} {self.auth_token}",
+        }
+    
     def test_order_list(self):
         """test order list endpoint."""
         url = reverse("order-list")
-        headers = {
-            "Authorization": f"{self.AUTH_TOKEN_PREFIX} {self.auth_token}",
-        }
-        response = self.client.get(path=url, content_type="application/json", headers=headers)
+        response = self.client.get(path=url, content_type="application/json", headers=self.headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        
+    def test_order_create(self):
+        """test order create endpoint."""
+        url = reverse("order-list")
+        response = self.client.post(path=url, headers=self.headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
