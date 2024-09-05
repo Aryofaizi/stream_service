f7cb3912f20b3411a0791610371e82cd186d88e7
aryo faizi <aryofaizi@gmail.com>
2024-09-05 19:54:33 +0330

    test_order_item_create modified , test_order_item_detail created new content created specifically for the test_order_item_create so  it doesn't conflict with the one created in the setUp

--- order/tests.py
+++ order/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from config.tests import AuthMixin
 from rest_framework import status
-from .models import Order
+from .models import Order, OrderItem
 from content.models import Content, Genre
 
 class OrderTest(AuthMixin, TestCase):
@@ -14,6 +14,7 @@ class OrderTest(AuthMixin, TestCase):
         }
         
         self.order = Order.objects.create(user=self.user)
+        self.order.save()
         # making content instance and genre
         self.genre = Genre.objects.create(
             title = "Animated",
@@ -32,6 +33,13 @@ class OrderTest(AuthMixin, TestCase):
         )
         self.content.genre.add(self.genre)
         self.content.save()
+        
+        self.order_item = OrderItem.objects.create(
+            order=self.order,
+            content=self.content,
+            unit_price=123456,
+        )
+        self.order_item.save()
     
     def test_order_list(self):
         """test order list endpoint."""
@@ -64,9 +72,29 @@ class OrderTest(AuthMixin, TestCase):
     def test_order_item_create(self):
         """test order item list endpoint."""
         url = reverse("order-item-list",kwargs={"order_pk":self.order.id})
+        # creating a specific content regardless of the one in the setUp
+        content = Content.objects.create(
+            title= "moana ",
+            description= "an animation for kids",
+            release_date= "2015-09-21",
+            category= "TVS",
+            rate= "3",
+            price= 120,
+        )
+        content.genre.add(self.genre)
+        content.save()
         """unit price in the payload is not important because it can be different based on the
          discounts or price from time to time on the website"""
-        payload = {"content":self.content.id,"unit_price":12345}
+        payload = {"content":content.id, "unit_price":12345}
         response = self.client.post(path=url, data=payload, content_type="application/json", headers=self.headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
    
+        
+    def test_order_item_detail(self):
+        """test order item detail endpoint."""
+        url = reverse("order-item-detail", kwargs={
+            "order_pk":self.order.id,
+            "pk": self.order_item.id
+        })
+        response = self.client.get(path=url, headers=self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
