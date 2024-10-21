c5eae60be85af139e4078dcc0f5a355265c34474
aryo faizi <aryofaizi@gmail.com>
2024-10-21 19:14:21 +0330

    SubscriptionPlan, UserSubscription models, migrations and admin created

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import SubscriptionPlan, UserSubscription
 
-# Register your models here.
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    model = SubscriptionPlan
+    list_display = [field.name for field in model._meta.fields]
+
+
+@admin.register(UserSubscription)
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    model = UserSubscription
+    list_display = [field.name for field in model._meta.fields]
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveBigIntegerField()
+    duration_in_days = models.PositiveIntegerField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+class UserSubscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
+    expire_date = models.DateTimeField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.plan
